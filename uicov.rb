234129abaf942feb328699d7491c5c210f076d34
Alexey Lyanguzov <alyanguzov@griddynamics.com>
2015-08-27 17:28:51 +0300

    Transition info

--- uicov.rb
+++ uicov.rb
@@ -32,7 +32,25 @@ end
 #
 ###########################
 class ScreenInfo
-  attr_reader :hits, :name, :missed
+  attr_reader :hits, :missed
+
+  def initialize(missed = false)
+    @hits = 0
+    @missed = missed
+  end
+
+  def hit
+    @hits = hits.succ
+    return self
+  end
+end
+
+class TransitionInfo
+  attr_reader :hits, :missed
+
+  def self.key(from, to, name)
+    [from.to_sym, to.to_sym, name.to_sym]
+  end
 
   def initialize(missed = false)
     @hits = 0
@@ -46,34 +64,47 @@ class ScreenInfo
 end
 
 class CoverageData
-  class << self
-    def screens
-      @screens ||= {}
-    end
+  def screens
+    @screens ||= {}
+  end
 
-    def add_screen(name)
-      screens[name] = ScreenInfo.new
-    end
+  def transitions
+    @transitions ||= {}
+  end
 
-    def hit_screen(name)
-      screen_info = screens.fetch(name, ScreenInfo.new(true))
-      screens[name] = screen_info.hit
-    end
+  def add_screen(name)
+    screens[name.to_sym] = ScreenInfo.new
+  end
+
+  def hit_screen(name)
+    name = name.to_sym
+    info = screens.fetch(name, ScreenInfo.new(true))
+    screens[name] = info.hit
+  end
+
+  def add_transition(from, to, name)
+    transitions[TransitionInfo.key(from, to, name)] = TransitionInfo.new
+  end
+
+  def hit_transition(from, to, name)
+    key = TransitionInfo.key(from, to, name)
+    info = transitions.fetch(key, TransitionInfo.new(true))
+    transitions[key] = info.hit
   end
 end
 
-CD = CoverageData
-
-#CoverageData.add_screen("A")
-#CoverageData.add_screen("B")
-#CoverageData.add_screen("A")
+CD = CoverageData.new
 
 def parse_model
+  #TODO: emulation of model reading
   %w[HomeScreen CheckoutScreen OneMoreScreen].each do |name|
     CD.add_screen name
   end
+  [%w[HomeScreen CheckoutScreen checkout], %w[CheckoutScreen OneMoreScreen one_more],
+   %w[OneMoreScreen CheckoutScreen checkout], %w[HomeScreen OneMoreScreen more]].each do |from, to, name|
+    CD.add_transition from, to, name
+  end
 end
-
 
 ###########################
 #
@@ -93,21 +124,27 @@ d "Using pattern file: #{PATTERN_FILE}"
 d "Unsing model file: #{MODEL_FILE}"
 d "Parsing log file: #{INPUT_LOG}"
 
-NEW_SCREEN_IS_SET_PATTERN = /\s+<==\s+([^ ]+)\s+is set as current screen/
+NEW_SCREEN_IS_SET_PATTERN = /1\s+<==\s+([^ ]+)\s+is set as current screen/
+TRANSITION_IS_DONE_PATTERN = /Transition '([^ ]+)'.*kind '(?:Tran|RegularAction).*from '([^ ]+)'.*to '([^ ]+)'/
+#TRANSITION_IS_DONE_PATTERN = /Transition '([^ ]+)'.*from '([^ ]+)'.*to '([^ ]+)'/
 
 parse_model #note that without the model only covered data is added but there is no info bout uncovered data
 
 File.open(INPUT_LOG).each do |line|
   case line
   when NEW_SCREEN_IS_SET_PATTERN
-    screen_name = $~[1]
-    CD.hit_screen screen_name
+    name = $~[1]
+    CD.hit_screen name
+  when TRANSITION_IS_DONE_PATTERN
+    from, to, name = $~[2], $~[3], $~[1]
+    CD.hit_transition from, to, name
   else
 #    d "--"
   end
 end
 
-puts CD.screens.to_yaml
+#pp CD.transitions
+puts CD.to_yaml
 
 puts "FINISH"
 
